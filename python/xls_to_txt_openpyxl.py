6e4d8bdd181b6eb6bee52ffdb1212dc8597185eb
Alexander Madorsky <16050565+madorskya@users.noreply.github.com>
2020-04-29 18:11:28 -0400

    added license headers to cpp sources

--- python/xls_to_txt_openpyxl.py
+++ python/xls_to_txt_openpyxl.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
+
+#    Excel to text files export script. Exports all tabs with a valid header.
+#    2020 Victor Barashko, University of Florida/Physics
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 import openpyxl as pyxl
 import os
 import sys
