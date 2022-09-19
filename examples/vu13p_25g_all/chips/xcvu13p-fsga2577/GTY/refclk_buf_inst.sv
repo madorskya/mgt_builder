3b9e6f8ab1acbdbba60267741d7bf65b7e44881b
Alexander Madorsky <16050565+madorskya@users.noreply.github.com>
2022-09-19 14:59:36 -0400

    moved manually created refclk_buf SV source into XLSX sources

--- examples/vu13p_25g_all/chips/xcvu13p-fsga2577/GTY/refclk_buf_inst.sv
+++ examples/vu13p_25g_all/chips/xcvu13p-fsga2577/GTY/refclk_buf_inst.sv
@@ -1,14 +1,14 @@
-			IBUFDS_GTE4 #
-			(
-				.REFCLK_EN_TX_PATH  (1'b0),
-				.REFCLK_HROW_CK_SEL (2'b00),
-				.REFCLK_ICNTL_RX    (2'b00)
-			) 
-			refclk_buf
-			(
-				.I     (refclk_p [gi]),
-				.IB    (refclk_n [gi]),
-				.CEB   (1'b0),
-				.O     (refclk   [gi]),
-				.ODIV2 ()
-			);
+			IBUFDS_GTE4 #		
+			(		
+				.REFCLK_EN_TX_PATH  (1'b0),	
+				.REFCLK_HROW_CK_SEL (2'b00),	
+				.REFCLK_ICNTL_RX    (2'b00)	
+			) 		
+			refclk_buf		
+			(		
+				.I     (refclk_p [gi]),	
+				.IB    (refclk_n [gi]),	
+				.CEB   (1'b0),	
+				.O     (refclk   [gi]),	
+				.ODIV2 ()	
+			);		
